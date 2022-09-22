1d6f5a65e5e1b3c258057548e66b741a4bd3093a
Sergey Afonin <afonin@d5.spb.ru>
2022-09-22 16:11:37 +0300

    script optimization, modx help script

---
+++ js/manager-help.js
@@ -0,0 +1,19 @@
+﻿Ext.onReady(function(){
+    let cmp = Ext.getCmp('modx-panel-resource');
+    if (myHelpConfig != undefined){
+        cmp.insert(1, {
+            layout: 'form',
+            header: false,
+            autoHeight: true,
+            collapsible: true,
+            items: [{
+                id: 'manager-help-panel',
+                html: myHelpConfig.text,
+                xtype: 'modx-description',
+                cls: 'main-wrapper'
+            }]
+        });
+    }
+    
+    cmp.doLayout();
+})
